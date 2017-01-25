5cbf3d8e0f931e77d74550a46e746fc0c8089dca
briand787b <briand787b@gmail.com>
2017-01-24 19:54:26 -0500

    chat now displays users correctly

--- chat.go
+++ chat.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"bufio"
 	"errors"
+	"bytes"
 )
 
 const (
@@ -103,24 +104,58 @@ func HandleAsServer(conn net.Conn) {
 
 func HandleIncoming(conn net.Conn, done chan <- struct{}) {
 	defer conn.Close()
-	io.Copy(os.Stdout, conn)
-	//input := bufio.NewScanner(conn)
-	//input.Split(bufio.ScanLines)
-	//for input.Scan() {
-	//	fmt.Println(input.Text())
-	//	fmt.Println("Received text from other")
-	//}
+	// io.Copy(os.Stdout, conn)
+
+
+	uname := ConfigureUser()
+	input := bufio.NewScanner(conn)
+	input.Split(bufio.ScanLines)
+	for input.Scan() {
+		fmt.Println("\r" + input.Text())
+		fmt.Print(uname)
+	}
 	done <- struct{}{}
 }
 
 func HandleOutgoing(conn net.Conn, done chan <- struct{}) {
 	defer conn.Close()
 	uname := ConfigureUser()
+	// fmt.Print(uname)
 	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
 		io.WriteString(conn, uname.String()+input.Text()+"\n")
+		fmt.Print(uname)
 	}
 	done <- struct{}{}
+}
+
+// ScanLines is a split function for a Scanner that returns each line of
+// text, stripped of any trailing end-of-line marker. The returned line may
+// be empty. The end-of-line marker is one optional carriage return followed
+// by one mandatory newline. In regular expression notation, it is `\r?\n`.
+// The last non-empty line of input will be returned even if it has no
+// newline.
+func ScanLines(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.IndexByte(data, '\n'); i >= 0 {
+		// We have a full newline-terminated line.
+		return i + 1, dropCR(data[0:i]), nil
+	}
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), dropCR(data), nil
+	}
+	// Request more data.
+	return 0, nil, nil
+}
+
+func dropCR(data []byte) []byte {
+	if len(data) > 0 && data[len(data)-1] == '\r' {
+		return data[0 : len(data)-1]
+	}
+	return data
 }
 
 func main() {
